2818ff0c78a705dfff770b64c15b2e0645f82bd9
firespark <fireleit@gmail.com>
2024-10-31 22:29:22 +0400

    feat: add prisma

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,13 @@
-FROM node:22.9-alpine AS builder
+FROM node:alpine
 
 WORKDIR /app
 
 COPY package*.json ./
 
-RUN npm ci cache clean --force --no-optional 
-
+RUN npm install --force
 
 COPY . .
 
 RUN npm run build
 
-FROM node:alpine
-ENV NODE_ENV=production
-
-WORKDIR /app
-
-COPY --from=builder /app/dist ./dist
-COPY --from=builder /app/node_modules ./node_modules
-
-CMD ["npm", "run", "start:dev"]
+CMD ["sh", "-c", "npx prisma migrate deploy && npm run start"]
--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { TrackModule } from './track/track.module';
 import { ArtistModule } from './artist/artist.module';
 import { AlbumModule } from './album/album.module';
 import { FavsModule } from './favs/favs.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UsersModule, TrackModule, ArtistModule, AlbumModule, FavsModule],
+  imports: [PrismaModule, UsersModule, TrackModule, ArtistModule, AlbumModule, FavsModule],
   controllers: [AppController],
   providers: [AppService],
 })
